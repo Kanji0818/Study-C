84d65f28281b5efb09b10f7dcf042f0fd8a4714d
Kanji0818 <kanji.ishizaki.1928@gmail.com>
2016-07-04 02:13:42 +0900

    階段

---
+++ lessonIF.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+	int x;
+	x = 3;
+
+
+	// input y
+	int y;
+	scanf("%d", &y);
+
+	if(y == 1){
+		printf("y is 1\n");
+	}
+	else if(y == 2){
+		printf("y is 2\n");
+	}
+	else if (y == 3){
+		printf("y is 3\n");
+	}
+	
+
+	else{
+		printf("y is NOT %d\n", x);
+
+	}
+	return 0;
+}
