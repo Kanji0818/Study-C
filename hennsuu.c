1b997c9019e2fd9d86d985880c676365471f30a9
Kanji0818 <kanji.ishizaki.1928@gmail.com>
2016-07-04 04:12:17 +0900

    配列

---
+++ hennsuu.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+
+	int a[10],i;
+	int sum=0;
+	int max=a[0];
+	int min=a[0];
+	
+	for(i=0; i<10; i++){
+		scanf("%d",&a[i]);
+		
+		sum +=a[i];
+	
+
+		if(a[i]>max){
+		max=a[i];
+		}
+
+		if(a[i]<min){
+		min=a[i];
+		}
+	}
+
+
+	printf("sum: %d\n",sum);
+	printf("max: %d\n",max);
+	printf("min: %d",min);
+
+	return 0;
+
+}
